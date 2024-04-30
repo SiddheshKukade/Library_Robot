0b0ff00539f24d711966928bbdcf71ca7dd96dcf
SiddheshKukade <littlelord124@gmail.com>
2024-04-30 17:21:48 +0530

    locatebook api

--- python_backend/api.py
+++ python_backend/api.py
@@ -2,11 +2,12 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from new_gpt import recommend
 import pandas as pd
-
+import serial
 app = Flask(__name__)
 
 # Enable CORS for the entire app
 CORS(app, resources={r"/recommend": {"origins": "http://localhost:3000"}})
+CORS(app, resources={r"/locatebook": {"origins": "http://localhost:3000"}})
 
 @app.route('/recommend', methods=['POST'])
 def get_recommendations():
@@ -19,6 +20,17 @@ def get_recommendations():
     print(recommendations_json)
 
     return jsonify(recommendations_json)
+
+
+@app.route('/locatebook', methods=['POST'])
+def get_locatebook():
+    data = request.get_json()
+    number = data.get('number')
+    # ser = serial.Serial('/dev/ttyACM0', 9600)
+    data = str(number) 
+    # ser.write(data.encode())
+    # ser.close()
+    return {"status": 200, "message": "Sucessful response", "number": data}
 
 if __name__ == '__main__':
     app.run(debug=True)
